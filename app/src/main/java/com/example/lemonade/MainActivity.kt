9a06567005080d7a65f74744eeb0a96c04c8e486
mvm11 <mvalenciaminota@gmail.com>
2022-03-09 20:40:15 -0500

    Primer interfaz de usuario terminada

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -95,19 +95,41 @@ class MainActivity : AppCompatActivity() {
         //  when the image is clicked we may need to change state to the next step in the
         //  lemonade making progression (or at least make some changes to the current state in the
         //  case of squeezing the lemon). That should be done in this conditional statement
+        when(lemonadeState) {
+            SELECT -> {
+                // TODO: When the image is clicked in the SELECT state, the state should become SQUEEZE
+                //  - The lemonSize variable needs to be set using the 'pick()' method in the LemonTree class
+                //  - The squeezeCount should be 0 since we haven't squeezed any lemons just yet.
+                lemonadeState = SQUEEZE
+                lemonSize = lemonTree.pick();
+                squeezeCount = 0
+            }
+            SQUEEZE -> {
+                // TODO: When the image is clicked in the SQUEEZE state the squeezeCount needs to be
+                //  INCREASED by 1 and lemonSize needs to be DECREASED by 1.
+                //  - If the lemonSize has reached 0, it has been juiced and the state should become DRINK
+                //  - Additionally, lemonSize is no longer relevant and should be set to -1
+                squeezeCount +1
+                lemonSize - 1
+                if(lemonSize == 0){
+                    lemonadeState = DRINK
+                    lemonSize = -1
+                }
 
-        // TODO: When the image is clicked in the SELECT state, the state should become SQUEEZE
-        //  - The lemonSize variable needs to be set using the 'pick()' method in the LemonTree class
-        //  - The squeezeCount should be 0 since we haven't squeezed any lemons just yet.
+            }
+            DRINK -> {
+                // TODO: When the image is clicked in the DRINK state the state should become RESTART
+            }
+            RESTART -> {
+                // TODO: When the image is clicked in the RESTART state the state should become SELECT
+            }
+        }
 
-        // TODO: When the image is clicked in the SQUEEZE state the squeezeCount needs to be
-        //  INCREASED by 1 and lemonSize needs to be DECREASED by 1.
-        //  - If the lemonSize has reached 0, it has been juiced and the state should become DRINK
-        //  - Additionally, lemonSize is no longer relevant and should be set to -1
 
-        // TODO: When the image is clicked in the DRINK state the state should become RESTART
 
-        // TODO: When the image is clicked in the RESTART state the state should become SELECT
+
+
+
 
         // TODO: lastly, before the function terminates we need to set the view elements so that the
         //  UI can reflect the correct state
